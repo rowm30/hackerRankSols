b9cb6c1f20fc927ce5bba79ba2883c37e101e927
KMV <rowm30@gmail.com>
2020-11-07 22:42:53 +0530

    selectionSortFunction

--- pickingNumbers.java
+++ pickingNumbers.java
@@ -1,5 +1,30 @@
 package hackerRankSols;
+import java.util.*;
 
 public class pickingNumbers {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        a = selectionSort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+    }
+
+    public static int[] selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j]<arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
